2439eea3921ed60503174f12678145bc7af4a61e
Brandon Hubbard <bkhubbard@gmail.com>
2016-11-01 16:30:59 -0700

    cleanup

--- modules/zillow/widgets/class-sales-widget.php
+++ modules/zillow/widgets/class-sales-widget.php
@@ -48,9 +48,8 @@ class ZillowPastSalesWidget extends WP_Widget {
 
 		$iframe_id = ! empty( $args['widget_id'] ) ? $args['widget_id'] : '';
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : '';
-		$screenname = ! empty( $instance['screenname'] ) ? $instance['screenname'] : '';
 		$zuid = ! empty( $instance['zuid'] ) ? $instance['zuid'] : '';
-		$format = ! empty( $instance['format'] ) ? $instance['format'] : '';
+
 
 		echo $args['before_widget'];
 
@@ -75,9 +74,7 @@ class ZillowPastSalesWidget extends WP_Widget {
 		// Set default values.
 		$instance = wp_parse_args( (array) $instance, array(
 			'title' => '',
-			'screenname' => '',
 			'zuid' => '',
-			'format' => 'normalWidget',
 		));
 
 		// Retrieve an existing value from the database.
@@ -90,12 +87,6 @@ class ZillowPastSalesWidget extends WP_Widget {
 		echo '<p>';
 		echo '	<label for="' . $this->get_field_id( 'title' ) . '" class="title-label">' . __( 'Tile:', 're-pro' ) . '</label>';
 		echo '	<input id="' . $this->get_field_id( 'title' ) . '" name="' . $this->get_field_name( 'title' ) . '" value="' . $title  . '" class="widefat">';
-		echo '</p>';
-
-		// Zillow Screenname.
-		echo '<p>';
-		echo '	<label for="' . $this->get_field_id( 'screenname' ) . '" class="title-label">' . __( 'Zillow Screenname:', 're-pro' ) . '</label>';
-		echo '	<input id="' . $this->get_field_id( 'screenname' ) . '" name="' . $this->get_field_name( 'screenname' ) . '" value="' . $screenname  . '" class="widefat">';
 		echo '</p>';
 
 		// Zillow User ID.
@@ -119,10 +110,7 @@ class ZillowPastSalesWidget extends WP_Widget {
 		$instance = $old_instance;
 
 		$instance['title'] = ! empty( $new_instance['title'] ) ? strip_tags( $new_instance['title'] ) : '';
-		$instance['screenname'] = ! empty( $new_instance['screenname'] ) ? strip_tags( $new_instance['screenname'] ) : '';
 		$instance['zuid'] = ! empty( $new_instance['zuid'] ) ? strip_tags( $new_instance['zuid'] ) : '';
-		$instance['format'] = ! empty( $new_instance['format'] ) ? strip_tags( $new_instance['format'] ) : '';
-		$instance['zmod'] = ! empty( $new_instance['zmod'] ) ? strip_tags( $new_instance['zmod'] ) : '';
 
 		return $instance;
 	}
