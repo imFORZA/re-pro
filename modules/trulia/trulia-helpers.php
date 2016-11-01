bac3f7a929e48514793b55c9f84cfca1818c998d
Brandon Hubbard <bkhubbard@gmail.com>
2016-11-01 14:02:37 -0700

    improve feed generator

--- modules/trulia/trulia-helpers.php
+++ modules/trulia/trulia-helpers.php
@@ -1,16 +1,23 @@
 <?php
 
 /**
- * Get Trulia RSS Listing Feed.
+ * Get Trulia RSS Listing Feed (https://www.trulia.com/tools/rss/).
  *
  * @access public
- * @param string $type (default: 'for_sale') Choose Type, for_sale or for_rent.
- * @param mixed $city City.
- * @param mixed $state State.
+ * @param string $type (default: 'for_sale')
+ * @param mixed $city
+ * @param mixed $state
+ * @param string $min_price (default: '')
+ * @param string $max_price (default: '')
+ * @param string $property_type (default: '')
+ * @param string $min_bath (default: '')
+ * @param string $max_bath (default: '')
+ * @param string $min_sqft (default: '')
+ * @param string $max_sqft (default: '')
  * @return void
  */
-function get_trulia_rss_listing_feed( $type = 'for_sale', $city, $state ) {
+function get_trulia_rss_listing_feed( $type = 'for_sale', $city, $state, $min_price = '', $max_price = '', $property_type = '', $min_bath = '', $max_bath = '', $min_sqft = '', $max_sqft = '' ) {
 
-	return 'https://www.trulia.com/rss2/'.$type.'/'.$city.','.$state.'/';
+	return esc_url( 'https://www.trulia.com/rss2/'.$type.'/'.$city.','.$state.'/' );
 
 }
