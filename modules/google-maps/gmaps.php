8029fdd39cd2be492c8537505e5510766ad837ef
Santiago Garza <santiago.garza@imforza.com>
2017-01-26 15:24:06 -0800

    Started google maps module integration

---
+++ modules/google-maps/gmaps.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * WP Google Maps API
+ *
+ * @package WP-Google-Maps-API
+ */
+ /*
+* Plugin Name: WP Google Maps API
+* Plugin URI: https://github.com/wp-api-libraries/wp-google-maps-api
+* Description: Perform API requests to Google Maps API.
+* Author: WP API Libraries, sgarza
+* Version: 1.0.0
+* Author URI: https://wp-api-libraries.com
+* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-google-maps-api
+* GitHub Branch: master
+*/
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! class_exists( 'GoogleMaps' ) ) {
+	/**
+	 * GreatSchools API Class.
+	 */
+	class GoogleMaps {
+
+		/**
+		 * API Key.
+		 *
+		 * @var string
+		 */
+		static private $api_key;
+
+		/**
+		 * Output Type.
+		 *
+		 * @var string
+		 */
+		static private $output;
+
+		/**
+		 * __construct function.
+		 *
+		 * @access public
+		 * @param mixed $api_key API Key
+		 * @param string $output (default: 'json') Output.
+		 * @return void
+		 */
+		public function __construct( $api_key) {
+			static::$api_key = $api_key;
+
+			add_action( 'wp_footer', array($this, 'footer'),  11);
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+		}
+
+		public function enqueue(){
+			wp_enqueue_script( 'wpapi-google-maps',plugins_url( 'google-maps.min.js', plugins_url( 'google-maps.min.js', __FILE__ ) ), array( 'jquery' ), null, true );
+		}
+
+		public function footer(){
+			// Only enqueue google maps API if yoast hasnt enqueued it already.
+			if( ! wp_script_is( 'maps-geocoder') ){
+				wp_enqueue_script( 'google-maps-api', 'https://maps.googleapis.com/maps/api/js?key=' . static::$api_key, array(), null );
+			}
+		}
+
+		public function print_map( $width, $height, $map_data ){
+			$map_data = apply_filters( 'wpapi_google_map_data', $map_data );
+			wp_localize_script( 'idxforza-maps', 'idxfListing', $map_data );
+
+			// Print Map
+			echo '<div id="listing-map"><div id="map-canvas" style="width: ' . $width . '; height: ' . $height . '"></div></div><!-- .listing-map -->';
+		}
+
+		public function shortcode( $atts ) {
+			// Set widget info.
+			$atts = shortcode_atts(
+				array(
+					'width' => '100%',
+					'height' => '300px',
+					'info' => 'title',
+					'lat' => '',
+					'lng' => '',
+				),
+				$atts, 'wp_google_maps'
+			);
+
+			$map_data = array(
+				'map_info_content' => $atts['info'],
+				'lat' => $atts['lat'],
+				'lng' => $atts['lng'],
+			);
+
+			$this->print_map( $atts['width'], $atts['height'], $map_data);
+		}
+	}
+
+}
+
+new GoogleMaps( 123 );
---
+++ modules/google-maps/google-maps.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function() {
+	if( typeof idxfListing !== 'undefined' ){
+		google.maps.event.addDomListener(window, 'load', initialize_map );
+	}
+});
+
+// Initialize map
+function initialize_map() {
+	var mapCanvas = document.getElementById('map-canvas');
+	var myLatLng = new google.maps.LatLng( idxfListing.lat , idxfListing.lng );
+	var map_style = JSON.parse(idxfListing.style);
+
+	var mapOptions = {
+		center: myLatLng,
+		zoom: 14,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: map_style
+	}
+
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		icon: '//s3.amazonaws.com/ae-plugins/wp-listings/images/active.png'
+	});
+
+	var infoContent =  idxfListing.map_info_content;
+	var infowindow = new google.maps.InfoWindow({
+		content: infoContent
+	});
+
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+	marker.setMap(map);
+	infowindow.open(map, marker);
+}
