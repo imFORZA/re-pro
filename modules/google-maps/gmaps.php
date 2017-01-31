8405cef42039d1f98e3f289793df6b2262049dec
Santiago Garza <santiago.garza@imforza.com>
2017-01-30 16:08:11 -0800

    phpcs fixes

--- modules/google-maps/gmaps.php
+++ modules/google-maps/gmaps.php
@@ -4,20 +4,22 @@
  *
  * @package WP-Google-Maps-API
  */
- /*
-* Plugin Name: WP Google Maps API
-* Plugin URI: https://github.com/wp-api-libraries/wp-google-maps-api
-* Description: Perform API requests to Google Maps API.
-* Author: WP API Libraries, sgarza
-* Version: 1.0.0
-* Author URI: https://wp-api-libraries.com
-* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-google-maps-api
-* GitHub Branch: master
+
+	/*
+	* Plugin Name: WP Google Maps API
+	* Plugin URI: https://github.com/wp-api-libraries/wp-google-maps-api
+	* Description: Perform API requests to Google Maps API.
+	* Author: WP API Libraries, sgarza
+	* Version: 1.0.0
+	* Author URI: https://wp-api-libraries.com
+	* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-google-maps-api
+	* GitHub Branch: master
 */
-/* Exit if accessed directly */
+
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 if ( ! class_exists( 'GoogleMaps' ) ) {
-	include_once('maps-widget.php');
+	include_once( 'maps-widget.php' );
 
 	/**
 	 * GreatSchools API Class.
@@ -42,45 +44,61 @@ if ( ! class_exists( 'GoogleMaps' ) ) {
 		 * __construct function.
 		 *
 		 * @access public
-		 * @param mixed $api_key API Key
-		 * @param string $output (default: 'json') Output.
-		 * @return void
+		 * @param String $api_key : API Key.
 		 */
-		public function __construct( $api_key) {
+		public function __construct( $api_key ) {
 			static::$api_key = $api_key;
 
-			add_action( 'wp_footer', array($this, 'footer'),  11);
+			add_action( 'wp_footer', array( $this, 'footer' ),  11 );
 			add_action( 'widgets_init', array( $this, 'register_widgets' ) );
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
 			add_shortcode( 'wp_google_maps', array( $this, 'shortcode' ) );
 		}
 
-		public function enqueue(){
+		/**
+		 * Enqueue JS.
+		 */
+		public function enqueue() {
 			wp_enqueue_script( 'wpapi-google-maps',plugins_url( 'google-maps.js', __FILE__ ), array( 'jquery' ), null, true );
 		}
 
-		public function footer(){
+		/**
+		 * Handle multiple google maps js api enqueues on the footer.
+		 */
+		public function footer() {
 			// Only enqueue google maps API if yoast hasnt enqueued it already.
-			if( ! wp_script_is( 'maps-geocoder') ){
+			if ( ! wp_script_is( 'maps-geocoder' ) ) {
 				wp_enqueue_script( 'google-maps-api', 'https://maps.googleapis.com/maps/api/js?key=' . static::$api_key, array(), null );
 			}
 		}
 
-		public static function print_map( $width, $height, $map_data ){
-      $default = array(
-        'lat' => '',
-        'lng' => '',
+		/**
+		 * Print dat map.
+		 *
+		 * @param  [String] $width    : Width of map.
+		 * @param  [String] $height   : Height of map.
+		 * @param  [Mixed]  $map_data : Array of map data to send to js.
+		 */
+		public static function print_map( $width, $height, $map_data ) {
+			$default = array(
+			'lat' => '',
+			'lng' => '',
 				'map_info_content' => '',
-        'style' => '[]',
-      );
+			'style' => '[]',
+			);
 
 			$map_data = apply_filters( 'wpapi_google_map_data', wp_parse_args( $map_data, $default ) );
 			wp_localize_script( 'wpapi-google-maps', 'idxfListing', $map_data );
 
-			// Print Map
-			echo '<div id="listing-map"><div id="map-canvas" style="width: ' . $width . '; height: ' . $height . '"></div></div><!-- .listing-map -->';
+			// Print Map.
+			echo '<div id="listing-map"><div id="map-canvas" style="width: ' . esc_attr( $width ) . '; height: ' . esc_attr( $height ) . '"></div></div><!-- .listing-map -->';
 		}
 
+		/**
+		 * Shortcode for printing a single map.
+		 *
+		 * @param  [type] $atts : shortcode attributes.
+		 */
 		public function shortcode( $atts ) {
 			// Set widget info.
 			$atts = shortcode_atts(
@@ -100,7 +118,7 @@ if ( ! class_exists( 'GoogleMaps' ) ) {
 				'lng' => $atts['lng'],
 			);
 
-			static::print_map( $atts['width'], $atts['height'], $map_data);
+			static::print_map( $atts['width'], $atts['height'], $map_data );
 		}
 
 		/**
