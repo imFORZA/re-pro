be1e48dd7b062a1503bb50e3da81f6815992d6fa
Santiago Garza <santiago.garza@imforza.com>
2017-02-16 16:17:14 -0800

    reverting requires

--- includes.php
+++ includes.php
@@ -11,21 +11,21 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 
 // Google maps.
-require_once( 'modules/google-maps/gmaps.php' );
+include_once( 'modules/google-maps/gmaps.php' );
 
 // Equal Housing.
-require_once( 'modules/equal-housing/equal-housing.php' );
+include_once( 'modules/equal-housing/equal-housing.php' );
 
 // Zillow.
-require_once( 'modules/zillow/zillow.php' );
+include_once( 'modules/zillow/zillow.php' );
 
-require_once( 'modules/trulia/trulia.php' );
+include_once( 'modules/trulia/trulia.php' );
 
-require_once( 'modules/inman/inman.php' );
+include_once( 'modules/inman/inman.php' );
 
-require_once( 'modules/homes-com/homes-com.php' );
+include_once( 'modules/homes-com/homes-com.php' );
 
 
-require_once( 'modules/streetadvisor/streetadvisor.php' );
+include_once( 'modules/streetadvisor/streetadvisor.php' );
 
-require_once( 'modules/greatschools/greatschools.php' );
+include_once( 'modules/greatschools/greatschools.php' );
