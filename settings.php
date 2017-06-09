def0c6bf434cdd102c4f1feb6f3db906add52bf5
Brandon Hubbard <bkhubbard@gmail.com>
2017-06-08 17:31:51 -0700

    Great Schools

--- settings.php
+++ settings.php
@@ -22,7 +22,7 @@ class REProSettings {
 	 */
 	public function __construct() {
 		if ( is_admin() ) {
-			$this->general_settings = get_option('re_pro_settings');
+			$this->general_settings = get_option('repro_settings');
 
 			add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
 			add_action( 'admin_init', array( $this, 'settings_init' ) );
@@ -40,42 +40,51 @@ class REProSettings {
 	 * Initialize settings fields and sections.
 	 */
 	public function settings_init() {
-		register_setting( 're_pro_settings', 're_pro_settings' );
+		register_setting( 'repro_settings', 'repro_settings' );
 
-		// Feeds.
+		// General Settings.
 		add_settings_section(
-			're_pro_settings',
+			'repro_settings',
 			__( 'General Settings', 're-pro' ),
 			array( $this, 'general_callback' ),
-			're_pro_general_settings'
+			'repro_general_settings'
 		);
 
 		add_settings_field(
 			'google_maps',
 			__( 'Google Maps Module', 're-pro' ),
 			array( $this, 'google_maps' ),
-			're_pro_general_settings',
-			're_pro_settings'
+			'repro_general_settings',
+			'repro_settings'
 		);
 
-		// TODO: Coming soon.
-		/*
+
 		add_settings_field(
-			'zillow',
-			__( 'Zillow Module', 're-pro' ),
+			'greatschools_apikey',
+			__( 'Great Schools API Key', 're-pro' ),
+			array( $this, 'greatschools_module' ),
+			'repro_general_settings',
+			'repro_settings'
+		);
+
+
+		add_settings_field(
+			'zillow_apikey',
+			__( 'Zillow API Key', 're-pro' ),
 			array( $this, 'zillow_module' ),
-			're_pro_general_settings',
-			're_pro_settings'
+			'repro_general_settings',
+			'repro_settings'
 		);
 
+
 		add_settings_field(
-			'sa_module',
-			__( 'Street Advisor Module', 're-pro' ),
+			'sa_apikey',
+			__( 'Street Advisor API Key', 're-pro' ),
 			array( $this, 'street_advisor' ),
-			're_pro_general_settings',
-			're_pro_settings'
+			'repro_general_settings',
+			'repro_settings'
 		);
-		*/
+
 	}
 
 	/**
@@ -98,36 +107,53 @@ class REProSettings {
 		$is_active = ( '1' === $gmaps_active ) ? 'Deactivate Google maps module?' : 'Activate Google maps module?';
 		$disable_zoom = checked( '1', $gmaps_zoom, false );
 
-		echo '<input style="vertical-align: top;" type="checkbox" name="re_pro_settings[gmaps_active]"' . esc_attr( $checked ) . ' value="1"> ';
+		echo '<input style="vertical-align: top;" type="checkbox" name="repro_settings[gmaps_active]"' . esc_attr( $checked ) . ' value="1"> ';
 		echo '<div style="display: inline-block;">';
 		esc_attr_e( $is_active );
 		echo '</div><br><br>';
 
-		echo '<input class="widefat" type="password" name="re_pro_settings[gmaps_key]" placeholder="Google maps API key" value="' . $gmaps_key . '">';
+		echo '<input class="widefat" type="password" name="repro_settings[gmaps_key]" placeholder="Google maps API key" value="' . $gmaps_key . '">';
 		echo '<span class="description"> Enter your google maps javascript api key</span><br><br>';
 
-		echo '<textarea style="width:500px;" rows="10" cols="50" name="re_pro_settings[gmaps_style]">';
+		echo '<textarea style="width:500px;" rows="10" cols="50" name="repro_settings[gmaps_style]">';
 		esc_attr_e( $gmaps_style );
 		echo '</textarea>';
 		echo '<br /><span class="description">Insert valid json to add custom style to maps. Json styles can be generated using <a target="_blank" href="https://snazzymaps.com/">Snazzy&nbsp;Maps</a> or the <a target="_blank" href="https://mapstyle.withgoogle.com/">Google&nbsp;Maps&nbsp;Styling&nbsp;Wizard</a></span><br><br>';
 
-		echo '<input style="vertical-align: top;" type="checkbox" name="re_pro_settings[gmaps_zoom]"' . esc_attr( $disable_zoom ) . ' value="1"> ';
+		echo '<input style="vertical-align: top;" type="checkbox" name="repro_settings[gmaps_zoom]"' . esc_attr( $disable_zoom ) . ' value="1"> ';
 		echo '<div style="display: inline-block;">';
 		echo 'Disable scroll zoom in Google Maps';
 		echo '</div>';
 	}
 
-	/**
-	 * Render lux portfoluo field.
-	 */
+
 	public function zillow_module() {
+
+		$zillow_apikey = isset( $this->general_settings['zillow_apikey'] ) ? $this->general_settings['zillow_apikey'] : '';
+
+		echo '<input class="widefat" type="text" name="repro_settings[zillow_apikey]" placeholder="Zillow API Key" value="' . $zillow_apikey . '">';
+
+	}
+
+	public function street_advisor() {
+
+		$sa_apikey = isset( $this->general_settings['sa_apikey'] ) ? $this->general_settings['sa_apikey'] : '';
+
+		echo '<input class="widefat" type="text" name="repro_settings[sa_apikey]" placeholder="Street Advisor API Key" value="' . $sa_apikey . '">';
 
 	}
 
 	/**
-	 * Render mayfair field.
+	 * greatschools_module function.
+	 *
+	 * @access public
+	 * @return void
 	 */
-	public function street_advisor() {
+	public function greatschools_module() {
+
+		$greatschools_apikey = isset( $this->general_settings['greatschools_apikey'] ) ? $this->general_settings['greatschools_apikey'] : '';
+
+		echo '<input class="widefat" type="text" name="repro_settings[greatschools_apikey]" placeholder="Great Schools API Key" value="' . $greatschools_apikey . '">';
 
 	}
 
@@ -136,15 +162,15 @@ class REProSettings {
 	 */
 	public function render_settings() {
 		if ( ! current_user_can( 'manage_options' ) ) {
-			wp_die( esc_attr( "You don't have access to this page", 'imforza' ) );
+			wp_die( esc_attr( "You don't have access to this page", 're-pro' ) );
 		}
 
 		echo '<div class="wrap">';
 		echo '<form method="post" action="options.php">';
-		echo '<h1>' . esc_attr( 'Real Estate Pro', 'imforza' ) . '</h1>';
+		echo '<h1>' . esc_attr( 'Real Estate Pro', 're-pro' ) . '</h1>';
 
-		settings_fields( 're_pro_settings' );
-		do_settings_sections( 're_pro_general_settings' );
+		settings_fields( 'repro_settings' );
+		do_settings_sections( 'repro_general_settings' );
 		submit_button();
 
 		echo '</form>';
